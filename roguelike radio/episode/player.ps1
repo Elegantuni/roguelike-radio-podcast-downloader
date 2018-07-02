e174d766d993745f96938058514b3cfd8b4889a1
Anthony Brown <groundup2360917182914017@gmail.com>
2018-07-02 15:16:24 -0400

    Add files via upload

---
+++ roguelike radio/episode/player.ps1
@@ -0,0 +1,74 @@
+If ($($args.Count) -eq 0)
+{
+	$start=1
+	$finish=-1
+	$position="00:00:00"
+}
+
+If ($($args.Count) -eq 2)
+{
+	$start=$args[0]
+	$finish=$args[1]
+	$position="00:00:00"
+}
+
+If ($($args.Count) -eq 3)
+{
+	$start=$args[0]
+	$finish=$args[1]
+	$position=$args[2]
+}
+
+If ($($args.Count) -eq !(0))
+{
+	If (($($args.Count) -lt 2) -or ($($args.Count) -gt 3))
+	{
+		echo "player.ps1 <start number> <finish number (-1 for infinite)>"
+
+		return 1
+	}
+}
+
+If ($finish -eq -1)
+{
+	cd $start
+	mpv roguelikeradio$start.mp3 --start $position
+	$determinetocontinue=$LastExitCode
+	If ($determinetocontinue -ne 0)
+	{
+		return 1
+	}
+	$start++
+	cd ..
+
+	While ($determinetocontinue -eq 0)
+	{
+		cd $start
+		mpv roguelikeradio$start.mp3
+		$determinetocontinue=$LastExitCode
+		If ($determinetocontinue -ne 0)
+		{
+			return 1
+		}
+		$start++
+		cd ..
+	}
+
+	return 0
+}
+
+If ($finish -ne -1)
+{
+	cd $start
+	mpv roguelikeradio$start.mp3 --start $position
+	cd ..
+	$start++
+
+	While ($start -le $finish)
+	{
+		cd $start
+		mpv roguelikeradio$start.mp3
+		cd ..
+		$start++
+	}
+}
