f6b0d836dfffbef13c12803083dd4569aa069440
Anthony Brown <groundup2360917182914017@gmail.com>
2018-12-11 17:31:53 -0500

    Add extra files

--- roguelike radio/episode/downloader.bash
+++ roguelike radio/episode/downloader.bash
@@ -1,14 +1,101 @@
 #!/bin/bash
 
-urlnumber=$1
-urlnumber2=$2
+if [ $# -eq 2 ] && [ $1 = "--all" ];
+then
+	urlnumber=1
+	urlnumber2=$2
+
+	while [ $urlnumber -le $urlnumber2 ]
+	do
+        	mkdir $urlnumber
+        	cd $urlnumber
+        	wget "http://www.gamesofgrey.com/roguelikeradio/roguelikeradio`echo $urlnumber`.mp3"
+        	cd ..
+        	let urlnumber=$urlnumber+1
+        	sleep 5
+	done
+
+        mkdir 6b_interview_Ido_Yehieli
+        cd 6b_interview_Ido_Yehieli
+        wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-ido.mp3
+        cd ..
+	sleep 5
+        mkdir 7b_spoilers_bonus_content_100_Rogues
+        cd 7b_spoilers_bonus_content_100_Rogues
+        wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio7-bonus.mp3
+        cd ..
+	sleep 5
+        mkdir 8b_interview_Brian_Walker_aka_Pender
+        cd 8b_interview_Brian_Walker_aka_Pender
+        wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-pender.mp3
+        cd ..
+	sleep 5
+        mkdir 9b_interview_with_Gaslamp_Games_developers_of_Dungeons_of_Dredmor
+        cd 9b_interview_with_Gaslamp_Games_developers_of_Dungeons_of_Dredmor
+        wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-gaslamp.mp3
+        cd ..
+	sleep 5
+        mkdir 40b_special_on_the_ADOM_IndieGoGo_Campaign
+        cd 40b_special_on_the_ADOM_IndieGoGo_Campaign
+        wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio40b-adom-special.mp3
+        cd ..
+	sleep 5
+        mkdir 110b_bonus_content_The_Red_Rooster_At_Dawn
+        cd 110b_bonus_content_The_Red_Rooster_At_Dawn
+        wget http://gamesofgrey.com/roguelikeradio/red-rooster-at-dawn.mp3
+	cd ..
+	sleep 5
+
+	exit 0
+fi
+
+if [ $# -eq 1 ] && [ $1 = "--extra" ];
+then
+	mkdir 6b_interview_Ido_Yehieli
+	cd 6b_interview_Ido_Yehieli
+	wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-ido.mp3
+	cd ..
+	sleep 5
+	mkdir 7b_spoilers_bonus_content_100_Rogues
+	cd 7b_spoilers_bonus_content_100_Rogues
+	wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio7-bonus.mp3
+	cd ..
+	sleep 5
+	mkdir 8b_interview_Brian_Walker_aka_Pender
+	cd 8b_interview_Brian_Walker_aka_Pender
+	wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-pender.mp3
+	cd ..
+	sleep 5
+	mkdir 9b_interview_with_Gaslamp_Games_developers_of_Dungeons_of_Dredmor
+	cd 9b_interview_with_Gaslamp_Games_developers_of_Dungeons_of_Dredmor
+	wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-gaslamp.mp3
+	cd ..
+	sleep 5
+	mkdir 40b_special_on_the_ADOM_IndieGoGo_Campaign
+	cd 40b_special_on_the_ADOM_IndieGoGo_Campaign
+	wget http://www.gamesofgrey.com/roguelikeradio/roguelikeradio40b-adom-special.mp3
+	cd ..
+	sleep 5
+	mkdir 110b_bonus_content_The_Red_Rooster_At_Dawn
+	cd 110b_bonus_content_The_Red_Rooster_At_Dawn
+	wget http://gamesofgrey.com/roguelikeradio/red-rooster-at-dawn.mp3
+	cd ..
+	sleep 5
+
+	exit 0
+fi
 
 if [ $# -ne 2 ];
 then
 	echo "downloader.bash <start number> <finish number>"
+	echo "downloader.bash --all <finish number>"
+	echo "downloader.bash --extra"
 
 	exit 1
 fi
+
+urlnumber=$1
+urlnumber2=$2
 
 while [ $urlnumber -le $urlnumber2 ]
 do
@@ -19,3 +106,6 @@ do
 	let urlnumber=$urlnumber+1
 	sleep 5
 done
+
+exit 0
+
--- roguelike radio/episode/downloader.ps1
+++ roguelike radio/episode/downloader.ps1
@@ -1,5 +1,53 @@
-$urlnumber = $args[0]
-$urlnumber2 = $args[1]
+If (($($args.Count) -eq 2) -and ($args[0] = "--all"))
+{
+	$urlnumber = 1
+	$urlnumber2 = $args[1]
+
+	While ($urlnumber -le $urlnumber2)
+	{
+        	mkdir $urlnumber
+        	cd $urlnumber
+        	wget.exe "http://www.gamesofgrey.com/roguelikeradio/roguelikeradio$urlnumber.mp3"
+        	cd ..
+        	$urlnumber++
+        	Start-Sleep 5
+	}
+	
+	mkdir 6b_interview_Ido_Yehieli
+        cd 6b_interview_Ido_Yehieli
+        wget.exe http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-ido.mp3
+        cd ..
+        Start-Sleep 5
+        mkdir 7b_spoilers_bonus_content_100_Rogues
+        cd 7b_spoilers_bonus_content_100_Rogues
+        wget.exe http://www.gamesofgrey.com/roguelikeradio/roguelikeradio7-bonus.mp3
+        cd ..
+        Start-Sleep 5
+        mkdir 8b_interview_Brian_Walker_aka_Pender
+        cd 8b_interview_Brian_Walker_aka_Pender
+        wget.exe http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-pender.mp3
+        cd ..
+        Start-Sleep 5
+        mkdir 9b_interview_with_Gaslamp_Games_developers_of_Dungeons_of_Dredmor
+        cd 9b_interview_with_Gaslamp_Games_developers_of_Dungeons_of_Dredmor
+        wget.exe http://www.gamesofgrey.com/roguelikeradio/roguelikeradio-gaslamp.mp3
+        cd ..
+        Start-Sleep 5
+	mkdir 40b_special_on_the_ADOM_IndieGoGo_Campaign
+        cd 40b_special_on_the_ADOM_IndieGoGo_Campaign
+        wget.exe http://www.gamesofgrey.com/roguelikeradio/roguelikeradio40b-adom-special.mp3
+        cd ..
+        Start-Sleep 5
+        mkdir 110b_bonus_content_The_Red_Rooster_At_Dawn
+        cd 110b_bonus_content_The_Red_Rooster_At_Dawn
+        wget.exe http://gamesofgrey.com/roguelikeradio/red-rooster-at-dawn.mp3
+        cd ..
+        Start-Sleep 5
+
+        return
+
+
+}
 
 If ($($args.Count) -ne 2)
 {
@@ -7,6 +55,9 @@ If ($($args.Count) -ne 2)
 	
 	return
 }
+
+$urlnumber = $args[0]
+$urlnumber2 = $args[1]
 
 While ($urlnumber -le $urlnumber2)
 {
